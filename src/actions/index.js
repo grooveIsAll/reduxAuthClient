50dd22c7430c6ac225a075a382b815aec17aa95c
Bobby Velasco <robert.m.velasco@gmail.com>
2016-05-27 18:50:32 -0700

    [feat]: complete and wire up sigin component, outline signin action creator

---
+++ src/actions/index.js
@@ -0,0 +1,14 @@
+
+
+export function signinUser({ email, password }) {
+  // Submit email/password to the server
+
+  // If request is good
+
+    // Update state to indicate the user is authenticated
+    // Save the JWT, so users can make an authenticated request in the future
+    // Redirect to route '/feature' 
+
+  // If request is bad
+    // Show an error
+}
--- src/index.js
+++ src/index.js
@@ -6,15 +6,16 @@ import { createStore, applyMiddleware } from 'redux';
 import { Router, Route, IndexRoute, browserHistory} from 'react-router';
 
 import App from './components/app';
+import Signin from './components/auth/signin';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <Router history={browserHistory}>
+    <Router history={browserHistory}> 
       <Route path="/" component={App}> 
-
+        <Route path="signin" component={Signin} />
       </Route>
     </Router>
   </Provider>
