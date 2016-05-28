493548dcaa9109edecf3bb8c8d9e5106689bc064
Bobby Velasco <robert.m.velasco@gmail.com>
2016-05-27 17:58:44 -0700

    [feat, library]: scaffold sigin form, use redux-form to connect to reducer, add redux-form dependency

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,12 @@
 import { combineReducers } from 'redux';
+import { reducer as form } from 'redux-form';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+    // dummie state
+  // state: (state = {}) => state
+
+  // 'form' prop of 'APP State' will be produced by 'redux-form' reducer, renamed 'form'
+  form
 });
 
 export default rootReducer;
