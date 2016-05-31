b702fde92d65fa54f936f3054cef3c47b37d2293
Bobby Velasco <robert.m.velasco@gmail.com>
2016-05-30 19:44:31 -0700

    [feat]: add signup component

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -4,11 +4,45 @@ import { reduxForm } from 'redux-form';
 import * as actions from './../../actions';
 
 class Signup extends Component {
+
+
   render() {
+    const { handleSubmit, fields: { email, password, passwordConfirm }} = this.props;
     return (
-      <div>Signup</div>
+      <form>
+        <fieldset className="form-group">
+          <label>
+            Email:
+          </label>
+            <input className="form-control" {...email} />
+        </fieldset>
+        <fieldset className="form-group">
+          <label>
+            Password:
+          </label>
+            <input className="form-control" type="password" {...password} />
+        </fieldset>
+        <fieldset className="form-group">
+          <label>
+            Confirm Password:
+          </label>
+            <input className="form-control" type="password" {...passwordConfirm} />
+        </fieldset>
+        <button action="submit" className="btn btn-primary">Sign Up!</button>
+      </form>
     );
   }
 }
 
-export default (Signup);
+function validate(formProps) {
+  console.log(formProps);
+  const errors = {};
+
+  return errors;  
+}
+
+export default reduxForm({
+  form: 'signup',
+  fields: ['email', 'password', 'passwordConfirm'],
+  validate 
+}, null, actions)(Signup);
