94c8c464e2dec246bf3b1298db88f1e942d6fadd
Bobby Velasco <robert.m.velasco@gmail.com>
2016-06-07 17:12:23 -0700

    [feat] create components for IndexRoute and feature content

---
+++ src/components/feature.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Feature extends Component {
+  render() {
+    return (
+      <div>This is our Feature!</div>
+    );
+  }
+}
+
+export default Feature;
